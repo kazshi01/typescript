cde6f84aec09698e91e60d0509950ccd1c156a57
kazshi01 <blumedio0619@gmail.com>
2024-05-20 14:34:32 +0900

    エラーハンドリングのファイル移動

--- express/src/app.ts
+++ express/src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import type { Express } from 'express';
-import type { Request, Response, NextFunction } from 'express';
+
+// // エラーハンドリングミドルウェアをインポート
+import errorHandler from './middlewares/errorHandler';
 
 // routesからCRUD操作をインポート(todoRotesは任意の名前)
 import todoRoutes from './routes/todo';
@@ -13,13 +15,8 @@ app.use(express.json());
 // todoRoutesを使用
 app.use('/', todoRoutes);
 
-// エラーハンドリングミドルウェア
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res
-    .status(err.status || 500)
-    .json({ error: err.message || 'Internal Server Error' });
-});
+// errorHandlerを使用
+app.use(errorHandler);
 
 // サーバーを起動
 app.listen(PORT, () => console.log(`server is runnnig on port ${PORT}`));
